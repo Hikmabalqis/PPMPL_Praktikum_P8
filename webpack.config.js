4884e4433a9662b7f8e07ced0f2ef447e4c01e5e
Balqis Hikmatullah <balqishikmatullah15@gmail.com>
2024-12-25 21:10:38 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app.mjs',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  mode: 'development'
+};
